2a43cf48b7ea8622b60f7eea6271a2f97bc14807
Alexander Kryshtalev <alex@voodoo.rocks>
2018-12-24 17:28:22 +0200

    Add flag NestedValidator::objectize to give the ability to convert nested arrays to the object

--- src/validators/NestedValidator.php
+++ src/validators/NestedValidator.php
@@ -8,11 +8,68 @@
 
 namespace vr\core\validators;
 
+use yii\base\DynamicModel;
+use yii\validators\Validator;
+
 /**
  * Class NestedValidator
  * @package app\modules\api\validators
  */
-class NestedValidator extends AssociatedArrayValidator
+class NestedValidator extends Validator
 {
+    /**
+     * @var
+     */
+    public $rules;
 
+    /**
+     * @var bool
+     */
+    public $objectize = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->message === null) {
+            $this->message = \Yii::t('yii', '{attribute} is invalid.');
+        }
+    }
+
+    /**
+     * @param \yii\base\Model $model
+     * @param string          $attribute
+     *
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        $attributes = $model->$attribute;
+
+        if (!is_array($attributes)) {
+            $this->addError($model, $attribute, $this->message, []);
+
+            return;
+        }
+
+        // Add attributes missing in the model but mentioned in rules
+        foreach ($this->rules as $rule) {
+            $ruleAttributes = is_array($rule[0]) ? $rule[0] : [$rule[0]];
+
+            foreach ($ruleAttributes as $ruleAttribute) {
+                $attributes = $attributes + [
+                        $ruleAttribute => null,
+                    ];
+            }
+        }
+
+        $dynamic = DynamicModel::validateData($attributes, $this->rules);
+        $model->addErrors($dynamic->errors);
+
+        if ($this->objectize) {
+            $model->$attribute = (object)$model->$attribute;
+        }
+    }
 }
